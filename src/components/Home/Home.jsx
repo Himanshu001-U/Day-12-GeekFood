ec522ba81a3f9c382f57a63c393983f6e6246952
Himanshu001-U <himanshuupadhayay2021@gmail.com>
2024-07-31 14:28:23 +0530

    Initial Commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import HeroSection from "./HeroSection"
+import DemoPage from "./DemoPage"
+import Comments from "./Comments"
+import React from "react"
+const Home = () => {
+    return (
+        <>
+        <HeroSection/>
+        <DemoPage />
+        <Comments />
+        </>
+    )
+}
+export default Home;
